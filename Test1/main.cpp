91f49e2dd4c2e026d569a6522a66bdd64ec014ae
FutbolniMyachik <ivanoff.ivan.kiborg@gmail.com>
2022-07-23 00:57:36 +0700

    Убран стандартный MainWindow. Добавлен пустой класс главного виджета

--- Test1/main.cpp
+++ Test1/main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
 #include <QMainWindow>
 
+#include "mainwidget.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QMainWindow w;
+    MainWidget mainWidget;
+    w.setCentralWidget(&mainWidget);
     w.show();
     return a.exec();
 }
